f387e6e06d871a736fc813766401367324eda25d
Dylan Tracey <dylan.tracey324@gmail.com>
2025-04-15 18:52:37 -0230

    Added POST form submission and redirects the user to view.html after submitting.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, url_for, redirect
 
 app = Flask(__name__)
 app.secret_key = "terminator-two"
@@ -15,7 +15,15 @@ def view():
 @app.route('/add', methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        pass
+        name = request.form.get("name")
+        muscle = request.form.get("muscle")
+        equipment = request.form.get("equipment")
+        instructions = request.form.get("instructions")
+
+        print(f"Name: {name}, Muscle(s): {muscle}, Equipment: {equipment}, Instructions: {instructions}")
+
+        return redirect(url_for("view"))
+
     return render_template("add.html")
 
 if __name__ == '__main__':
