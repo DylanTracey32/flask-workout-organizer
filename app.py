3e6e34207a44f168bff247fbcf52a8daeaf02d97
Dylan Tracey <dylan.tracey324@gmail.com>
2025-04-15 17:10:52 -0230

    Added view.html to eventually view our cards there and a route on app.py to the page.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
+app.secret_key = "terminator-two"
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/view')
+def view():
+    return render_template("view.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
